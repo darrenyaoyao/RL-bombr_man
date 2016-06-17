ee450985764303feb06949eed6769c06d1cb4a45
darrenyaoyao <b02901042@ntu.edu.tw>
2016-06-17 22:53:38 +0800

    Config change

--- train/bombrtrain.py
+++ train/bombrtrain.py
@@ -12,48 +12,10 @@ class bombrtrain:
       self.models_init()
 
    def models_init(self):
-      self.train_model = build_model()
-      self.value_model = build_model()
+      #Todo
 
-   def build_model(self):
-      state_model = self.build_state_model()
-      action_model = self.build_action_model()
-
-      return self.build_final_model(state_model, action_model)
-
-   def build_state_model(self):
-      state_model = Sequential()
-      state_model.add(Convolution2D(64, 3, 3, border_mode='same', 
-         input_shape=(1, BOMBR_COLUMN, BOMBR_ROW)), activation='relu')
-      state_model.add(Convolution2D(64, 3, 3, activation='relu')
-      state_model.add(MaxPooling2D(pool_size=(2, 2)))
-      state_model.add(Dropout(0.25))
-      state_model.add(Flatten())
-      state_model.add(Dense(128, activation='relu'))
-
-      return state_model
-
-   def build_action_model(self):
-      action_model = Sequential()
-      action_model.add(Dense(32, input_dim=10))
-
-      return action_model
-
-   def build_final_model(self, state_model, action_model):
-      merge = Merge([state_model, action_model], mode="concat")
-      final_model = Sequential()
-      final_model.add(merge)      
-      final_model.add(Dense(128, activation='relu'))
-      final_model.add(Dense(1))
-
-      return final_model
-   
-   def value_based_training(self):
-      # DQN Todo
-      #self.train_model.compile()
-
-   def dqn_mse_target(self):
-      #DQN Todo
+   def data_init(self):
+      #Todo
       
 
 
