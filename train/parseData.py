e1686003dd48696eff31c5feebc0f99f16b69ea7
alex84726 <b02901053@ntu.edu.tw>
2016-06-27 23:54:08 +0800

    reward+-1 classified

--- train/parseData.py
+++ train/parseData.py
@@ -5,7 +5,7 @@ BOMBR_ROW = 19
 FINALSTATE = np.full((19,19),3.0)
 #FINALSTATE = np.full(361,3.0)
 REWARD = 0
-ACTION_PERCENT_RETAIN = 0.2
+#ACTION_PERCENT_RETAIN = 0.2
 
 class parseData:
     def __init__(self, option):
@@ -13,8 +13,29 @@ class parseData:
         self.reward_file = option.reward
         self.save_state = option.state
         self.save_action = option.action
-        self.seq = option.seq
+        self.save_seq = option.seq
+        self.save_classify = option.classify
         self.data_init()
+
+    def classified_train_data(self):
+        #classifed['X'][0]:states
+        #classifed['X'][1]:actions
+        self.classified = {'+1': [[],[]], '-1': [[],[]] }
+        flag = 0
+        for i in range(len(self.sequence)):
+            flag = self.sequence[i][-1]['Rt1']
+            for j in range(len(self.sequence[i])):
+                 if flag == int(-1):
+                     if j < 70:
+                         self.classified['-1'][0].append(self.sequence[i][-j-1]['St'])
+                         self.classified['-1'][1].append(self.sequence[i][-j-1]['At'])
+                     else:
+                         self.classified['+1'][0].append(self.sequence[i][-j-1]['St'])
+                         self.classified['+1'][1].append(self.sequence[i][-j-1]['At'])
+                 else:
+                     self.classified['+1'][0].append(self.sequence[i][-j-1]['St'])
+                     self.classified['+1'][1].append(self.sequence[i][-j-1]['At'])
+
 
     def policy_train_data(self):
         self.states = []
@@ -23,12 +44,15 @@ class parseData:
         action0[0] = 1
         for i in range(len(self.sequence)):
             for j in range(len(self.sequence[i])):
+                if j < 70:
+                    ACTION_PERCENT_RETAIN = 0.1
+                else:
+                    ACTION_PERCENT_RETAIN = 0.25
                 if ((self.sequence[i][j]['At']==action0).all()) and (random.random() > ACTION_PERCENT_RETAIN):
                     pass
                 else:
                     self.states.append(self.sequence[i][j]['St'])
                     self.actions.append(self.sequence[i][j]['At'])
-        print len(self.states)
 
     def getDataDistribution(self):
         try:
@@ -40,8 +64,8 @@ class parseData:
             print("call parse function first")
 
     def check_duplicate(self, data):
-        if (data[-1]['St'] == data[-2]['St']).all() and (data[-1]['At'] == data[-2]['At']).all() and (data[-1]['Rt1'] == data[-2]['Rt1']):
-            data.pop(-2)
+        if (data[-3]['St'] == data[-2]['St']).all() and (data[-3]['At'] == data[-2]['At']).all() and (data[-3]['Rt1'] == data[-2]['Rt1']):
+            data.pop(-3)
             return True
         else:
             return False
@@ -51,37 +75,30 @@ class parseData:
         ObserFile = open(self.obser_file,'r')
         trash = ObserFile.readline()
         RewardFile = open(self.reward_file,'r')
-        #every game sequence's data
         self.sequence = list()
         for r in RewardFile.readlines():
             strings = ObserFile.readline()
             ori_data = self.spliter(strings, "  ")
-            #length_separate = len(ori_data)/5
-            #separate each seq into five segment
-            #every time step's data
             data = list()
             counter = 0
             for d in ori_data:
-                timeslice = np.fromstring(d, sep = ",")
-                timeslice = timeslice.astype(int)
-                action=np.zeros(10)
+                timeslice = (np.fromstring(d, sep = ",")).astype(int)
+                action = np.zeros(10)
                 action[timeslice[0]] = 1
                 if len(timeslice) != 1:
                     state = np.reshape( timeslice[1:], (BOMBR_ROW, BOMBR_COLUMN))
-                    #state = timeslice[1:]
                     info = {'St':state , 'Rt1':REWARD}
                     if counter != 0:
                         data[counter-1]['At'] = action
                         data[counter-1]['St1'] = state
+                    data.append(info)
                 else:
                     data[counter-1]['At'] = action
                     data[counter-1]['St1'] = FINALSTATE
-                    rew = int(r.replace("\n",""))
-                    data[counter-1]['Rt1'] = rew
-                if len(data) > 1:
+                    data[counter-1]['Rt1'] = int(r.replace("\n",""))
+                if len(data) > 2:
                     if(self.check_duplicate(data)):
                         counter = counter-1
-                data.append(info)
                 counter = counter+1
             self.sequence.append(data)
 
@@ -89,11 +106,20 @@ class parseData:
         data = string.split(splitElm)
         data.pop()
         return data
+    
+    def save_classify(self):
+        classify = np.asarray(self.classified)
+        if self.save_classify != None:
+            np.save(self.save_classify, classify)
 
     def save_npy(self):
         states = np.asarray(self.states)
         actions = np.asarray(self.actions)
+        if self.save_state != None and self.save_action != None:
+            np.save(self.save_state, states)
+            np.save(self.save_action, actions)
+    
+    def save_sequence(self):
         sequence = np.asarray(self.sequence)
-        np.save(self.save_state, states)
-        np.save(self.save_action, actions)
-        np.save(self.seq, sequence)
+        if self.save_seq != None:
+            np.save(self.save_seq, sequence)
