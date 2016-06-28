8e1d7e496e01edcc91833172fa43e422f00d85b5
darrenyaoyao <b02901042@ntu.edu.tw>
2016-06-28 17:20:36 +0800

    pass

--- dqn.bash
+++ dqn.bash
@@ -1,1 +1,1 @@
-python2 ./train/main.py -O ai_simulate_1.obser -R ai_simulate_1,reward -Q sequence.npy
+python2 ./train/main.py -O ../data/ai_simulate_1.obser -R ../data/ai_simulate_1,reward -Q sequence.npy
